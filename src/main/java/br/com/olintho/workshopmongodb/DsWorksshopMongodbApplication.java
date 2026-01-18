e6beff4b8c46eb57d06a55c6b448da5392471f46
Olintho Jesus <olinthojesus@yahoo.com.br>
2026-01-18 02:01:13 -0300

    Initializing project

---
+++ src/main/java/br/com/olintho/workshopmongodb/DsWorksshopMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.olintho.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsWorksshopMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsWorksshopMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/olintho/workshopmongodb/DsWorksshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.olintho.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsWorksshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

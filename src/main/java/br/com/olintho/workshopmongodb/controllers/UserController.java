86a3fd31f03c6251c3263f8bb0e8ed737affee6a
Olintho Jesus <olinthojesus@yahoo.com.br>
2026-01-18 02:56:00 -0300

    Implementing User Model

--- src/main/java/br/com/olintho/workshopmongodb/controllers/UserController.java
+++ src/main/java/br/com/olintho/workshopmongodb/controllers/UserController.java
@@ -1,14 +1,29 @@
 package br.com.olintho.workshopmongodb.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.olintho.workshopmongodb.model.User;
+
+
 @RestController
+@RequestMapping(value = "/users")
 public class UserController {
 
-	@GetMapping("/")
-	public String home() {
-		System.out.println("Controlador");
-		return "Teste";
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		
+		List<User> list = new ArrayList<>();
+		User roseli = new User("1", "Roseli", "roseli@rdm.com.br");
+		User bela = new User("2", "Bela", "bela@rdm.com.br");
+		list.addAll(Arrays.asList(roseli, bela));
+		
+		return ResponseEntity.ok().body(list);
 	}
 }

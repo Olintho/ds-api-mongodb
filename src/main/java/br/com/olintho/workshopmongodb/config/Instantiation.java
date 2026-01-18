a6b49e4f592095ca21e52e644a422ee6cf4e55eb
Olintho Jesus <olinthojesus@yahoo.com.br>
2026-01-18 15:49:44 -0300

    Refactoring user Class

---
+++ src/main/java/br/com/olintho/workshopmongodb/config/Instantiation.java
@@ -0,0 +1,33 @@
+package br.com.olintho.workshopmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.olintho.workshopmongodb.model.User;
+import br.com.olintho.workshopmongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+
+		User user = new User(null, null, null, null, null);
+		User roseli = new User(null, "Roseli", "roseli@rdm.com.br", "(11) 9 8765-4321", "First Street");
+		User bela = new User(null, "Bela", "bela@rdm.com.br", "(11) 9 8765-4322", "Second Street");
+		User deda = new User(null, "Deda", "deda@rdm.com.br", "(11) 9 8765-4323", "Third Street");
+		User le = new User(null, "Leticia", "le@rdm.com.br", "(11) 9 8765-4324", "Fourth Street");
+		
+		userRepository.saveAll(Arrays.asList(roseli, deda, bela, le));
+	}
+
+	
+}

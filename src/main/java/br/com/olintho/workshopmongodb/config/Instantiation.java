6b38aa21e843f32931c1f74b5752dad33f202841
Olintho Jesus <olinthojesus@yahoo.com.br>
2026-02-01 00:54:06 -0300

    Adding comments at posts

--- src/main/java/br/com/olintho/workshopmongodb/config/Instantiation.java
+++ src/main/java/br/com/olintho/workshopmongodb/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.olintho.workshopmongodb.dto.AuthorDTO;
+import br.com.olintho.workshopmongodb.dto.CommentDTO;
 import br.com.olintho.workshopmongodb.model.Post;
 import br.com.olintho.workshopmongodb.model.User;
 import br.com.olintho.workshopmongodb.repository.PostRepository;
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("10/01/2026"), "Partiu férias", "Estou indo para Cancun. Abraços!", new AuthorDTO(bela));
 		Post post2 = new Post(null, sdf.parse("05/01/2026"), "Bom dia", "Acordei ótimamente ótima hoje!", new AuthorDTO(bela));
+		
+		CommentDTO comment1 = new CommentDTO("Boa viagem maninha!", sdf.parse("10/01/2026"), new AuthorDTO(deda));
+		CommentDTO comment2 = new CommentDTO("Aproveita bem as férias!!!", sdf.parse("10/01/2026"), new AuthorDTO(roseli));
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia e que DEUS te acompanhe", sdf.parse("05/01/2026"), new AuthorDTO(roseli));
+		
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().addAll(Arrays.asList(comment3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
